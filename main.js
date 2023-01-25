bb811ae2febbdb8dd405e6536460f68340dc6e8a
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2023-01-25 18:20:25 +0900

    add:styleなど追加

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-document.getElementById("app").innerHTML = `
+//要素の取得
+const target = document.getElementById("app");
+
+//HTMLの挿入
+target.innerHTML = `
+<div class="layout">
 <h1>element.innerHTMLとテンプレートリテラルで掴むReactの動き方</h1>
 <p>こんにちは世界。</p>
+<ul class="list">
+  <li class="listItem">
+    <h2>サムネイル</h2>
+    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+  </li>
+  <li>
+    <h2>サムネイル</h2>
+    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+  </li>
+  <li>
+    <h2>サムネイル</h2>
+    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+  </li>
+  <li>
+    <h2>サムネイル</h2>
+    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+  </li>
+  <li>
+    <h2>サムネイル</h2>
+    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+  </li>
+  <li>
+    <h2>サムネイル</h2>
+    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+  </li>
+</ul>
+</div>
 `;
