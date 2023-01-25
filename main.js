0fd6fa4b3058566c162e0bdd844aa3e6f91d48e3
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2023-01-25 19:05:32 +0900

    update:title切り出し

--- main.js
+++ main.js
@@ -2,35 +2,43 @@
 const target = document.getElementById("app");
 
 //HTMLの挿入
-target.innerHTML = `
-<div class="layout">
-<h1>element.innerHTMLとテンプレートリテラルで掴むReactの動き方</h1>
-<p>こんにちは世界。</p>
-<ul class="list">
-  <li class="listItem">
-    <h2>サムネイル</h2>
-    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
-  </li>
-  <li>
-    <h2>サムネイル</h2>
-    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
-  </li>
-  <li>
-    <h2>サムネイル</h2>
-    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
-  </li>
-  <li>
-    <h2>サムネイル</h2>
-    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
-  </li>
-  <li>
-    <h2>サムネイル</h2>
-    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
-  </li>
-  <li>
-    <h2>サムネイル</h2>
-    <p>サンプルテキストサンプルテキストサンプルテキスト</p>
-  </li>
-</ul>
-</div>
-`;
+const Title = () => {
+  return `
+    <h1>element.innerHTMLとテンプレートリテラルで掴むReactの動き方</h1>
+  `;
+};
+
+const Layout = () => {
+  return `
+  <div class="layout">
+    ${Title()}
+    <ul class="list">
+      <li class="listItem">
+        <h2>見出し</h2>
+        <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+      </li>
+      <li>
+        <h2>見出し</h2>
+        <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+      </li>
+      <li>
+        <h2>見出し</h2>
+        <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+      </li>
+      <li>
+        <h2>見出し</h2>
+        <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+      </li>
+      <li>
+        <h2>見出し</h2>
+        <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+      </li>
+      <li>
+        <h2>見出し</h2>
+        <p>サンプルテキストサンプルテキストサンプルテキスト</p>
+      </li>
+    </ul>
+  </div>`;
+};
+
+target.innerHTML = Layout();
